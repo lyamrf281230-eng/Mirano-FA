d286d10e56cdda145b2f719f8702d1072703d0b6
lyamrf281230-eng <lyamrf281230@gmail.com>
2026-09-21 18:18:07 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log("=================================");
+    console.log("          MIRANO FA SHOP");
+    console.log("=================================");
+    console.log("Site : http://localhost:3000");
+});
